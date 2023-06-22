7e57a0f890324e94d756624670ca5296b5e4c4f6
Samantha Lopez <samanthacat120@gmail.com>
2023-06-21 21:15:51 -0400

    create a bug

--- music21ex.py
+++ music21ex.py
@@ -1,7 +1,7 @@
 #Ex1.py - Loading work and saving it to MIDI file
 import music21
 sBach = music21.corpus.parse('bach/bwv7.7')
-mf = music21.midi.translate.streaToMidiFile(sBach)
+mf = music21.midi.translate.streamToMidiFile(sBach)
 mf.open("bach.mid", "wb")
 mf.write()
-mf.close()
+mf.open()
