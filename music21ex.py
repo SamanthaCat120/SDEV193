babe142b24e797a125041ddf9f36b6163c8d74e4
Samantha Lopez <samanthacat120@gmail.com>
2023-06-21 20:53:16 -0400

    create music21 example

---
+++ music21ex.py
@@ -0,0 +1,7 @@
+#Ex1.py - Loading work and saving it to MIDI file
+import music21
+sBach = music21.corpus.parse('bach/bwv7.7')
+mf = music21.midi.translate.streaToMidiFile(sBach)
+mf.open("bach.mid", "wb")
+mf.write()
+mf.close()
