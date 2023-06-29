777564f5ac6d1a8c784b1ffe56fba332b688d16e
Samantha Lopez <samanthacat120@gmail.com>
2023-06-28 20:01:02 -0400

    Add requirements.txt
    Add requirements.txt and this includes the bach.midi file that was produced with the music21ex.py.
    
    no issues yet

--- music21ex.py
+++ music21ex.py
@@ -1,7 +1,12 @@
-#Ex1.py - Loading work and saving it to MIDI file
+#Ex1.py - Loading work and saving it to MIDI file from https://www.youtube.com/watch?v=7W00dYfMpmw
+#import module
 import music21
+
+#retrieve Bach bwv7.7
 sBach = music21.corpus.parse('bach/bwv7.7')
+
 mf = music21.midi.translate.streamToMidiFile(sBach)
 mf.open("bach.mid", "wb")
 mf.write()
 mf.close()
+
